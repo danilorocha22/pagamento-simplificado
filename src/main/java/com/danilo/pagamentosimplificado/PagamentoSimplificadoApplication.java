5931a34730120d8aa3b1d5b26bd34d1748112a36
Danilo Rocha <danilo.rochaa@gmail.com>
2024-10-02 15:38:23 -0300

    Configuração inicial do projeto

---
+++ src/main/java/com/danilo/pagamentosimplificado/PagamentoSimplificadoApplication.java
@@ -0,0 +1,12 @@
+package com.danilo.pagamentosimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentoSimplificadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagamentoSimplificadoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/danilo/pagamentosimplificado/PagamentoSimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.pagamentosimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoSimplificadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
