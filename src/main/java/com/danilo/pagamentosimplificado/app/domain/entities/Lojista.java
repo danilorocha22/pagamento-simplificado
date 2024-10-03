b5111d7d85910696b476c188da71621c41d73b56
Danilo Rocha <danilo.rochaa@gmail.com>
2024-10-02 21:03:24 -0300

    feat: Adicionando validação para CNPJ

--- src/main/java/com/danilo/pagamentosimplificado/app/domain/entities/Lojista.java
+++ src/main/java/com/danilo/pagamentosimplificado/app/domain/entities/Lojista.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CNPJ;
 
 @Getter
 @Setter
@@ -17,5 +18,6 @@ public class Lojista extends Usuario {
 
     @NotBlank
     @Column(unique = true)
+    @CNPJ(message = "CNPJ inválido")
     private String cnpj;
 }
