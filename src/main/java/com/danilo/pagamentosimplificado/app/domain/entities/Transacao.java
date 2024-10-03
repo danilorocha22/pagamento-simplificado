f619278daa2618c64667bb748d4a777ec3cb47d1
Danilo Rocha <danilo.rochaa@gmail.com>
2024-10-02 21:02:04 -0300

    feat: Criação da entidade Transacao

---
+++ src/main/java/com/danilo/pagamentosimplificado/app/domain/entities/Transacao.java
@@ -0,0 +1,52 @@
+package com.danilo.pagamentosimplificado.app.domain.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import static jakarta.persistence.CascadeType.REFRESH;
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static java.util.UUID.randomUUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "transacoes")
+@EqualsAndHashCode(of = "id")
+public class Transacao implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String codigo;
+
+    @NotNull
+    @DecimalMin(value = "0.01", message = "O valor da transação deve ser maior que 0")
+    private BigDecimal valor;
+
+    @NotNull
+    @JoinColumn(name = "pagador_id")
+    @ManyToOne(cascade = REFRESH, fetch = LAZY)
+    private Carteira pagador;
+
+    @NotNull
+    @JoinColumn(name = "recebedor_id")
+    @ManyToOne(cascade = REFRESH, fetch = LAZY)
+    private Carteira recebedor;
+
+    @PrePersist
+    private void gerarCodigoTransacao() {
+        this.setCodigo(randomUUID().toString());
+    }
+}
