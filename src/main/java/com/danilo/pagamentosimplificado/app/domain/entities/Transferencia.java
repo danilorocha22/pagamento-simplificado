3fc5fd556c80025271acc239fd562154a68c7856
Danilo Rocha <danilo.rochaa@gmail.com>
2024-10-03 00:02:09 -0300

    feat: Criação de validações de transação

--- src/main/java/com/danilo/pagamentosimplificado/app/domain/entities/Transferencia.java
+++ src/main/java/com/danilo/pagamentosimplificado/app/domain/entities/Transferencia.java
@@ -1,5 +1,6 @@
 package com.danilo.pagamentosimplificado.app.domain.entities;
 
+import com.danilo.pagamentosimplificado.app.domain.exceptions.TransferenciaException;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
@@ -14,6 +15,7 @@ import java.math.BigDecimal;
 import static jakarta.persistence.CascadeType.REFRESH;
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static java.math.BigDecimal.ZERO;
 import static java.util.UUID.randomUUID;
 
 @Getter
@@ -48,5 +50,17 @@ public class Transferencia implements Serializable {
     @PrePersist
     private void gerarCodigoTransacao() {
         this.setCodigo(randomUUID().toString());
+    }
+
+    public void validarValorTransferido() {
+        if (this.getValor().compareTo(ZERO) <= 0) {
+            throw new TransferenciaException("O valor da transação deve ser maior que 0");
+        }
+    }
+
+    public void impedirLogistaRealizar() {
+        if (this.getPagador().getUsuario().isLogista()) {
+            throw new TransferenciaException("Apenas usuários comuns podem realizar uma transferência");
+        }
     }
 }
