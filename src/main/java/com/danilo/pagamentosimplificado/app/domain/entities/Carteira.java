b2d01150c0af5f38f6bee6f3920166c72c677313
Danilo Rocha <danilo.rochaa@gmail.com>
2024-10-02 23:57:54 -0300

    feat: Adicionar ou debitar saldo na Carteira

--- src/main/java/com/danilo/pagamentosimplificado/app/domain/entities/Carteira.java
+++ src/main/java/com/danilo/pagamentosimplificado/app/domain/entities/Carteira.java
@@ -1,5 +1,6 @@
 package com.danilo.pagamentosimplificado.app.domain.entities;
 
+import com.danilo.pagamentosimplificado.app.domain.exceptions.TransferenciaException;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
@@ -11,6 +12,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static java.math.BigDecimal.ZERO;
 
 @Getter
 @Setter
@@ -26,10 +28,29 @@ public class Carteira implements Serializable {
     private Long id;
 
     @NotNull
-    private BigDecimal saldo = BigDecimal.ZERO;
+    private BigDecimal saldo = ZERO;
 
     @NotNull
     @OneToOne
     @JoinColumn(name = "usuario_id")
     private Usuario usuario;
+
+    public void adicionarSaldo(BigDecimal valor) {
+        this.saldo = this.saldo.add(valor);
+    }
+
+    public void debitarSaldo(BigDecimal valor) {
+        this.validarSaldo(valor);
+        this.saldo = this.saldo.subtract(valor);
+    }
+
+    public void validarSaldo(BigDecimal valor) {
+        if (isInsuficiente(valor)) {
+            throw new TransferenciaException("Saldo insuficiente");
+        }
+    }
+
+    private boolean isInsuficiente(BigDecimal valor) {
+        return this.saldo.compareTo(ZERO) == 0 || this.saldo.subtract(valor).compareTo(ZERO) < 0;
+    }
 }
