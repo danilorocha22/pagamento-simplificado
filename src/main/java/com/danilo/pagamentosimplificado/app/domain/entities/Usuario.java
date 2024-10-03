d3f7e879323efe42f9c81b0e078b09a70462ef9a
Danilo Rocha <danilo.rochaa@gmail.com>
2024-10-02 21:02:39 -0300

    feat: Refatoração da entidade Usuario

--- src/main/java/com/danilo/pagamentosimplificado/app/domain/entities/Usuario.java
+++ src/main/java/com/danilo/pagamentosimplificado/app/domain/entities/Usuario.java
@@ -1,7 +1,10 @@
 package com.danilo.pagamentosimplificado.app.domain.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -28,12 +31,23 @@ public abstract class Usuario implements Serializable {
     private Long id;
 
     @NotBlank
+    @Size(min = 5, message = "O nome deve ter no mínimo 5 letras")
     private String nomeCompleto;
 
     @NotBlank
     @Column(unique = true)
+    @Email(message = "E-mail inválido")
     private String email;
 
     @NotBlank
     private String senha;
+
+    @NotNull
+    @OneToOne(mappedBy = "usuario")
+    private Carteira carteira;
+
+    public Usuario() {
+        this.carteira = new Carteira();
+        this.carteira.setUsuario(this);
+    }
 }
