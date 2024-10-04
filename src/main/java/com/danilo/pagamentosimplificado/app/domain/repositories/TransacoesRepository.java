c8d2c3a862a2d7debf5ff81433a495025e55efeb
Danilo Rocha <danilo.rochaa@gmail.com>
2024-10-03 21:19:47 -0300

    feat: Criação do repositório de Transações

--- src/main/java/com/danilo/pagamentosimplificado/app/domain/repositories/TransacoesRepository.java
+++ src/main/java/com/danilo/pagamentosimplificado/app/domain/repositories/TransacoesRepository.java
@@ -3,5 +3,5 @@ package com.danilo.pagamentosimplificado.app.domain.repositories;
 import com.danilo.pagamentosimplificado.app.domain.entities.Transferencia;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
+public interface TransacoesRepository extends JpaRepository<Transferencia, Long> {
 }
